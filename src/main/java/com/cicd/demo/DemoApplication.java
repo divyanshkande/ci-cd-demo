a396af6aeb44a7fe92c72b8e879149d039417876
Divyansh Kande <156500307+divyanshkande@users.noreply.github.com>
2026-09-24 16:47:37 +0530

    Add version endpoint

--- src/main/java/com/cicd/demo/DemoApplication.java
+++ src/main/java/com/cicd/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.cicd.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,5 +10,9 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	@GetMapping("/version")
+public String version() {
+    return "v1.0";
+}
 
 }
