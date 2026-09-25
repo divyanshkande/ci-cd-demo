1d4e78071edeee6fef5bbd1936706efa0ea6a888
Divyansh Kande <156500307+divyanshkande@users.noreply.github.com>
2026-09-25 23:34:01 +0530

    Added docker automation

--- src/main/java/com/cicd/demo/DemoApplication.java
+++ src/main/java/com/cicd/demo/DemoApplication.java
@@ -3,16 +3,18 @@ package com.cicd.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-	@GetMapping("/version")
-public String version() {
-    return "v1.0";
-}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
+    @GetMapping("/version")
+    public String version() {
+        return "v1.0";
+    }
 }
